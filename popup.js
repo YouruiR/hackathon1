b19dbc40493a168a7b9606765517a0233ce41209
Yourui <yourui.ruan@gmail.com>
2023-01-21 13:01:30 -0500

    using storage for list
    Co-authored-by: Djaworski1 <Djaworski1@users.noreply.github.com>

--- popup.js
+++ popup.js
@@ -1,5 +1,3 @@
-// import { getCurrentTab } from "./background.js"
-
 function example() {
     console.log('hello')
 }
@@ -11,32 +9,67 @@ function getURL() {
         });
   }
   
-  //   console.log(button)
-  
+let titleList = [];
+
   document.addEventListener("DOMContentLoaded", async function () {
-    // const button = document.getElementById("storage");
-    // button.setAttribute("id", "newButton")   ;
-    // button.onclick = example;
-    // button.appendChild(document.createTextNode('press me'));
-    // document.body.appendChild(button)
-    const button = document.getElementById('storage')
-    // const currentTab = await getCurrentTab();
+    getTitleListAndRestoreDom();
+    const button = document.getElementById('storage');
+
     button.addEventListener("click", getCurrentTab, false);
 
-    // button.onclick = (function(){
-    //     chrome.runtime.sendMessage({ msg: "startFunc" });
-    //  }, false);
-      
+    
+}, false);
 
-    async function getCurrentTab() {
-        let queryOptions = { active: true, currentWindow: true };
-        // `tab` will either be a `tabs.Tab` instance or `undefined`.
-        let [tab] = await chrome.tabs.query(queryOptions);    
-        console.log(tab.title);
-      }
+async function getCurrentTab() {
+    let queryOptions = { active: true, currentWindow: true };
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let [tab] = await chrome.tabs.query(queryOptions);
+    
+    const title = tab.title;
+    if (titleList.indexOf(title) === -1) {
+        addTitleToListAndSave(title);
+        addTitleToDom(title);
+    }
+    // let item = document.createElement("li");
+    // item.appendChild(document.createTextNode(`${tab.title}`));
+    // ul.appendChild(item);
+    // domList.push(tab.title)
+}
+
+function getTitleListAndRestoreDom() {
+    chrome.storage.local.get({titleList: []}, function(data) {
+        titleList = data.titleList;
+        titleList.forEach(function(title) {
+            addTitleToDom(title)
+        })
+    })
+}
+
+function addTitleToDom(title) {
+    let ul = document.getElementById("list");
+      const item = document.createElement("li");
+      const addedItem = document.createElement('a');
+      addedItem.textContent = title;
+      item.appendChild(addedItem);
+      ul.appendChild(item);
+    }
+
+  function addTitleToListAndSave(title) {
+    if(titleList.indexOf(title) === -1){
+        //Title is not already in list
+        titleList.push(title);
+        saveTitleList();
+    }
+  }
 
 
-  }, false);
+  function saveTitleList(callback) {
+    chrome.storage.local.set({titleList}, function() {
+        if (typeof callback === 'function') {
+            callback();
+        }
+    })
+  } 
 
 
 
